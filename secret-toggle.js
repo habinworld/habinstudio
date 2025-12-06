14fac9bbb711db85c995b31334402d37d17105eb
habinworld <kguiog@gmail.com>
2025-12-06 22:25:59 +0900

    Update secret-toggle.js

--- secret-toggle.js
+++ secret-toggle.js
@@ -11,6 +11,10 @@ document.addEventListener('DOMContentLoaded', () => {
   if (!toggle || !box) return;
 
   toggle.addEventListener('click', () => {
+     /* ⭐ 관리자면 비밀문 절대 안 열림 */
+    if (localStorage.getItem("habin_admin") === "true") {
+      return;   // ← 클릭 무시 (열리지 않음) 
+       }
     box.style.display = (box.style.display === 'block') ? 'none' : 'block';
   });
 
