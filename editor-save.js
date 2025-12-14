e68a238000a04a4a00024bbb3d3bea21d3d33060
habinworld <kguiog@gmail.com>
2025-12-15 00:47:44 +0900

    Update editor-save.js

--- editor-save.js
+++ editor-save.js
@@ -89,10 +89,10 @@ function updatePost() {
      버튼 연결
   ============================ */
   btnSave &&
-  btnSave.addEventListener("click", () => {
-    window.POST_MODE === "new"  && saveNew();
-    window.POST_MODE === "edit" && updatePost();
-  });
+btnSave.addEventListener("click", () => {
+  window.POST_ID ? updatePost() : saveNew();
+});
+
 
   if (btnDelete) {
     btnDelete.addEventListener("click", deletePost);
