01b0b4698339052232fe25b1d1ad2b71f2f0e67d
habinworld <kguiog@gmail.com>
2025-12-26 21:05:25 +0900

    Update image-input.js

--- image-input.js
+++ image-input.js
@@ -1,1 +1,14 @@
+// image-input.js
+(function () {
+  const input = document.getElementById("hb-image-input");
+  if (!input || window.__HB_IMAGE_INPUT_BOUND__) return;
+
+  input.addEventListener("change", e => {
+    const file = e.target.files && e.target.files[0];
+    file && window.EditorCore && EditorCore.insertImage(file);
+    e.target.value = "";
+  });
+
+  window.__HB_IMAGE_INPUT_BOUND__ = true;
+})();
 
