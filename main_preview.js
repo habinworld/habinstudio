8a4f5c1099022b5acd3d3620b1eb5074ca100f59
habinworld <kguiog@gmail.com>
2026-01-15 10:06:40 +0900

    Update main_preview.js

--- main_preview.js
+++ main_preview.js
@@ -28,11 +28,11 @@
   );
   if (!currentPage || currentPage < 1) currentPage = 1;
 
-  const totalPages = Math.ceil(posts.length / ITEMS_PER_PAGE);
+  const totalPages = Math.ceil(posts.length / itemsPerPage);
 
   function getPagePosts() {
-    const start = (currentPage - 1) * ITEMS_PER_PAGE;
-    return posts.slice(start, start + ITEMS_PER_PAGE);
+    const start = (currentPage - 1) * itemsPerPage;
+    return posts.slice(start, start + itemsPerPage);
   }
 
   /* =========================
