99b508efb3a071287890b20f13fee6ab0d3fdc59
habinworld <kguiog@gmail.com>
2026-01-18 00:37:10 +0900

    Refactor image insertion logic in image-input.js

--- image-input.js
+++ image-input.js
@@ -12,8 +12,7 @@
 
   input.addEventListener("change", e => {
     const file = e.target.files && e.target.files[0];
-    ImageEngine.insert(file); 
-    file && window.EditorCore && EditorCore.insertImage(file);
+   file && window.EditorCore && EditorCore.insertImage(file);
     e.target.value = "";
   });
 })();
