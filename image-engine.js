4cabe9de1ab2575d0cde6e30ca14e10890ef0592
habinworld <kguiog@gmail.com>
2026-01-21 01:16:28 +0900

    Update image-engine.js

--- image-engine.js
+++ image-engine.js
@@ -255,16 +255,15 @@ function enableFreeMove(box) {
      - 저장된 DOM 구조를 다시 그림
      - box / img 구조 복구
   =================================================== */
-  // 🔒 box 크기 복원 (핵심)
+   function renderAll() {
+  editor.querySelectorAll(".hb-img-box").forEach(box => {
+    const id = box.dataset.imgId;
+    if (!id) return;
+     // 🔒 box 크기 복원 (핵심)
 if (box.dataset.w && box.dataset.h) {
   box.style.width  = box.dataset.w + "px";
   box.style.height = box.dataset.h + "px";
 } 
-  function renderAll() {
-  editor.querySelectorAll(".hb-img-box").forEach(box => {
-    const id = box.dataset.imgId;
-    if (!id) return;
-
     let img = box.querySelector("img");
     if (!img) {
       img = document.createElement("img");
