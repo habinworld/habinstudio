efe516c2ad1ed75b2fdf6b44047022a0b3696755
habinworld <kguiog@gmail.com>
2026-02-01 22:33:26 +0900

    Update image-engine.js

--- image-engine.js
+++ image-engine.js
@@ -265,14 +265,14 @@ if (box.dataset.w && box.dataset.h) {
   box.style.height = box.dataset.h + "px";
 } 
   // 🔑 리사이즈 핸들 복원 (EDIT 핵심)
-    if (!box.querySelector(".hb-resize-handle")) {
-      addResizeHandles(box);
-    }
+    box.querySelectorAll(".hb-resize-handle").forEach(h => h.remove());
+addResizeHandles(box);
+    
      // 🔑 EDIT에서 box 선택 가능하게 클릭 이벤트 복구
   box.addEventListener("click", e => {
   e.stopPropagation();
   selectBox(box);
-});
+};
     let img = box.querySelector("img");
     if (!img) {
       img = document.createElement("img");
